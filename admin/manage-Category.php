fca94503c417f7342541fb3d52f096aba8fc6682
nithyanand <nithyanand.nrenterprises@gmail.com>
2021-11-15 00:22:17 +0530

    Part 2

--- admin/manage-Category.php
+++ admin/manage-Category.php
@@ -4,6 +4,7 @@
                 <div class="wrapper">
                <h1> Manage Category</h1>
 
+                  
               
             </div>
             </div>
--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -6,7 +6,50 @@
                 <div class="wrapper">
                <h1> Manage Admin</h1>
 
-              
+                <!-- button -->
+                <a href="#" class="btn-primary">Add Admin</a>
+
+               <table class ="tbl-full">
+
+                        <tr>
+                            <th>S.N.</th>
+                            <th>Full Name</th>
+                            <th>Username</th>
+                            <th>Actions</th>
+
+                        </tr>
+
+                        <tr>
+                            <td>1.</td>
+                            <td>nithyannd</td>
+                            <td>nithy</td>
+                            <td>
+                                update admin
+                                delete admin
+                            </td>
+
+                        </tr>
+                        <tr>
+                            <td>2.</td>
+                            <td>nithyannd</td>
+                            <td>nithy</td>
+                            <td>
+                                update admin
+                                delete admin
+                            </td>
+
+                        </tr>
+                        <tr>
+                            <td>3.</td>
+                            <td>nithyannd</td>
+                            <td>nithy</td>
+                            <td>
+                                update admin
+                                delete admin
+                            </td>
+
+                        </tr>
+                </table>
             </div>
             </div>
             <!-- main Content Secton End -->
