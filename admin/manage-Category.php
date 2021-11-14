20f67a883cba5db2ebb3448858aad990e4d3265a
nithyanand <nithyanand.nrenterprises@gmail.com>
2021-11-14 23:14:42 +0530

    New 1

--- admin/manage-Category.php
+++ admin/manage-Category.php
@@ -1,5 +1,11 @@
 <?php include('partials/menu.php'); ?>
 
+<div class ="main-contant">
+                <div class="wrapper">
+               <h1> Manage Category</h1>
 
+              
+            </div>
+            </div>
 
 <?php include('partials/footer.php'); ?>
